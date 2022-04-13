0089d20e82390a377a1e1a190ce195a9db4b028d
sebcl <sebastianclavijo123@gmail.com>
2022-04-12 19:02:20 -0500

    file formatting

---
+++ utils/players_season_builder.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import openpyxl
+import os
+from pathlib import Path
+
+
+if __name__ == "__main__":
+    # Create intial Dataframe
+
+    # Read all Excels within a directory
+
+    # Clean Combined Dataframe
